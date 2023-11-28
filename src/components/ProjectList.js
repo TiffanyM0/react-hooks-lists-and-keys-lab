ffb79ef86db381d0ad56a92cc22e785ef0508adf
TIFFANY MAINA <tiffmaina@gmail.com>
2023-11-28 19:51:32 +0300

    mid-way

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,17 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectsElements = projects.map((project)=>{
+    return (<ProjectItem key={project.id} name={projects.name} about={projects.about} technologies={projects.technologies}/> )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {/* {projects.map((project)=>{<ProjectItem key={project.id}/>})} */}
+        {projectsElements}
+      </div>
     </div>
   );
 }
